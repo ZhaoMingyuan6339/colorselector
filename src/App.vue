50114ee44f7e5358a636a815c32ca04c96a617c1
CoderMikasa <1309446363@qq.com>
2021-05-18 16:52:12 +0800

    完成了一个颜色选择器

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <button @click="switchFormat">切换颜色格式</button>
+    <div class="current-color">当前颜色：{{color}}</div>
+    <ck-color-picker v-model="color" :color-format="format" :show.sync="show"/>
+  </div>
+</template>
+
+<script>
+import CkColorPicker from "@/components/ColorSelector";
+
+export default {
+  name: "app",
+
+  components: {
+    CkColorPicker
+  },
+
+  data() {
+    return {
+      color: "#c83aaa",
+      show: true,
+      format: "hex"
+    };
+  },
+
+  methods: {
+    switchFormat() {
+      this.format = this.format === "hex" ? "rgb" : "hex";
+    }
+  }
+};
+</script>
+
+<style lang='stylus'>
+$mc = #409eff;
+
+#app {
+  margin: 50px;
+
+  button {
+    background: #fff;
+    padding: 4px 16px;
+    margin-bottom: 12px;
+    border: 1px solid #bbbbbb;
+    border-radius: 4px;
+    font-size: 15px;
+    cursor: pointer;
+    outline: none;
+    margin-right: 20px;
+    color: #555;
+
+    &:hover {
+      border-color: $mc;
+      color: $mc;
+    }
+  }
+
+  .current-color {
+    margin-bottom: 24px;
+  }
+}
+</style>
